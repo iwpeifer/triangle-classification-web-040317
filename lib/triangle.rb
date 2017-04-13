f5223291c87b98e533b22e6f0fc87b1d15d7215f
iwpeifer <iwpeifer@gmail.com>
2017-04-13 19:30:04 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+	attr_accessor :l1, :l2, :l3
+  
+  def initialize(l1, l2, l3)
+  	@sides = [l1, l2, l3]
+  end
+
+  def valid?
+    if @sides.any?{|s| s <= 0} || @sides.sort[0] + @sides.sort[1] <= @sides.sort[2]
+      return false
+    else
+      return true
+    end
+  end
+
+  def kind
+    if !self.valid?
+        raise TriangleError
+  	elsif @sides.uniq.size == 1
+  		return :equilateral
+  	elsif @sides.uniq.size == 2
+  		return :isosceles
+  	elsif @sides.size == @sides.uniq.size
+  		return :scalene
+  	end
+  end
+end
+
+class TriangleError < StandardError
+	def message
+		"Triangle is whack."
+	end
 end
